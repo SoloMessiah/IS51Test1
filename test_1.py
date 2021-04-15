c3a7337ae7fc12667d8e5e041fbcd771914d434a
SoloMessiah <ch1n0ul33@gmail.com>
2021-04-15 15:06:24 -0700

    Finished structured English

---
+++ test_1.py
@@ -0,0 +1,15 @@
+"""
+
+Our program will figure out which payment option, in terms of dollars, will give us the most return.
+The first option, is getting paid 100 dollars per day, for ten days.
+The second option is getting paid one dollar the first day, but doubling the amount for a total of 10 days.
+We will use funtions, named funtion1 and function2 respectively, to calculate the total amount for each option.
+
+The first function, function1 will give us $100 * 10 days.
+The second function, function2 will use a loop that occurs 10 times, with the amount being doubled per loop, and finally getting the sum of the 10 loops.
+
+If the first option is better, the program will output to the user "Option 1 is better."
+If the second option is better, the program will output to the user "Option 2 is better."
+If both option one and option two are equal, the program will output to the user "Option 1 and Option 2 pays the same."
+
+"""
